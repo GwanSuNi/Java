8d7084f8819fb715d3e7fe3595ced91e7f13639a
GwanSuNi <tony2yoo@gmail.com>
2022-06-28 11:56:05 +0900

    [add] Added Default Variable Class

---
+++ Summer01-01/src/com/java/ex/MainClassVariable.java
@@ -0,0 +1,30 @@
+package com.java.ex;
+
+public class MainClassVariable {
+
+	public static void main(String[] args) {
+		boolean boolVar = true;
+		char charVar = 'c';
+
+		byte byteVar = 127;
+		short shortVar = 128;
+		int intVar = 3;
+		long longVar = -4;
+
+		float floatVar = 1.1f;
+		double doubleVar = 2.2;
+		
+		
+		System.out.println(boolVar);
+		System.out.println(charVar);
+
+		System.out.println(byteVar);
+		System.out.println(shortVar);
+		System.out.println(intVar);
+		System.out.println(longVar);
+		
+		System.out.println(floatVar);
+		System.out.println(doubleVar);
+	}
+
+}
