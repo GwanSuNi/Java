a5c70cfe1e590b2a4cf7aa846d92d7c5de9e1cb9
GwanSuNi <tony2yoo@gmail.com>
2022-09-26 10:47:36 +0900

    [update] Recovered to abstract class
    errors were annoying

--- 2022-4-01/src/com/java/ex/car/Motors.java
+++ 2022-4-01/src/com/java/ex/car/Motors.java
@@ -1,12 +1,12 @@
 package com.java.ex.car;
 
-interface Motors {
+public abstract class Motors {
 	// 추상 클래스는 추상 메소드 한 개 이상을 갖고 있으며
 	// 추상 메서드는 메서드의 구현부를 작성하지 않음
-	public void buyK3();
-	public void buyK5();
-	public void buyK7();
-	public void buySportage();
-	public void buySorento();
+	public abstract void buyK3();
+	public abstract void buyK5();
+	public abstract void buyK7();
+	public abstract void buySportage();
+	public abstract void buySorento();
 	
 }
